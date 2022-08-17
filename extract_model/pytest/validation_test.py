afdedf74603b30eb05f0131450cca097474d6f61
Yun KyungJun <sound605@gmail.com>
2022-08-17 11:12:37 +0900

    Update validation_test.py

--- extract_model/pytest/validation_test.py
+++ extract_model/pytest/validation_test.py
@@ -53,23 +53,58 @@ def extract_part_func(x, y):
 
     return result, result1
 
-def compare_func(result, result2):
+def compareValue_func(result, result2):
     # 10의 -6승까지 비교
     return np.count_nonzero((np.abs(result[0]-result2[0])>0.000001).astype(int))
 
-def compare_full_func(result, result2):
+def compareFull_func(result, result2):
     # 전체 값 비교
     return np.count_nonzero(result[0] - result2[0])
+
+def compare_inputName(session, session_div1):
+    return session.get_inputs()[0].name, session_div1.get_inputs()[0].name
+
+def compare_inputShape(session, session_div1):
+    return session.get_inputs()[0].shape, session_div1.get_inputs()[0].shape
+
+def compare_inputType(session, session_div1):
+    return session.get_inputs()[0].type, session_div1.get_inputs()[0].type
+
+def compare_outputName(session, session_div2):
+    return session.get_outputs()[0].name, session_div1.get_outputs()[0].name
+
+def compare_outputShape(session, session_div2):
+    return session.get_outputs()[0].shape, session_div1.get_outputs()[0].shape
+
+def compare_outputType(session, session_div2):
+    return session.get_outputs()[0].type, session_div1.get_outputs()[0].type
 
 # ------------------------------------------------------------------------------------------
 # Function Test
 # ------------------------------------------------------------------------------------------
 
-def test_compare():
+def test_compareValue():
     res1, res2 = extract_func(session, session_div1, session_div2)
-    assert compare_func(res1, res2) == 0
+    assert compareValue_func(res1, res2) == 0
 
-def test_compare_part():
-    res1, res2 = extract_part_func(session, session_div1)
-    assert compare_func(res1, res2) == 0
-    assert compare_full_func(res1, res2) == 0
+def test_compare_inputParameters():
+    nameResult1 , nameResult2 = compare_inputName(session, session_div1)
+    shapeResult1, shapeResult2 = compare_inputShape(session, session_div1)
+    typeResult1, typeResult2 = compare_inputType(session, session_div1)
+
+    assert nameResult1 == nameResult2
+    assert shapeResult1 == shapeResult2
+    assert typeResult1 == typeResult2
+
+def test_compare_outputParameters():
+    nameResult1 , nameResult2 = compare_outputName(session, session_div2)
+    shapeResult1 , shapeResult2 = compare_outputShape(session, session_div2)
+    typeResult1 , typeResult2 = compare_outputType(session, session_div2)
+
+    assert nameResult1 == nameResult2
+    assert shapeResult1 == shapeResult2
+    assert typeResult1 == typeResult2
+
+
+
+
