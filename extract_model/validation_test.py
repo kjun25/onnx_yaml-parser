f4049c0ecc1539ab0b18891e0c69f7426bde9feb
Yun KyungJun <sound605@gmail.com>
2022-08-17 10:49:28 +0900

    Update validation_test.py
    add test case

--- extract_model/validation_test.py
+++ extract_model/validation_test.py
@@ -2,11 +2,10 @@ import numpy as np
 import onnxruntime as ort
 
 # ONNXRuntime functions
-session = ort.InferenceSession('mxnet_exported_resnet18.onnx',  providers=['CPUExecutionProvider'])
-session_div1 = ort.InferenceSession('test1.onnx',  providers=['CPUExecutionProvider'])
-session_div2 = ort.InferenceSession('test2.onnx',  providers=['CPUExecutionProvider'])
+session = ort.InferenceSession('F:/kjYun/2022/07_onnx/mxnet_exported_resnet18.onnx',  providers=['CPUExecutionProvider'])
+session_div1 = ort.InferenceSession('F:/kjYun/2022/07_onnx/test1.onnx',  providers=['CPUExecutionProvider'])
+session_div2 = ort.InferenceSession('F:/kjYun/2022/07_onnx/test2.onnx',  providers=['CPUExecutionProvider'])
 
-# Validate extract_function
 def extract_func(x, y, z):
     input_name = x.get_inputs()[0].name
     input_shape = x.get_inputs()[0].shape
@@ -32,9 +31,45 @@ def extract_func(x, y, z):
 
     result2 = z.run([output_name2], {input_name2: result1[0]})
 
-    # 10의 -6승까지 비교
-    return np.count_nonzero((np.abs(result[0]-result2[0])>0.000001).astype(int));
+    return result, result2
 
-# test
-def test_answer():
-    assert extract_func(session, session_div1, session_div2) == 0
+def extract_part_func(x, y):
+    input_name = x.get_inputs()[0].name
+    input_shape = x.get_inputs()[0].shape
+    input_type = x.get_inputs()[0].type
+    output_name = x.get_outputs()[0].name
+
+    input_name1 = y.get_inputs()[0].name
+    input_shape1 = y.get_inputs()[0].shape
+    input_type1 = y.get_inputs()[0].type
+    output_name1 = y.get_outputs()[0].name
+
+    data = np.ones(input_shape, dtype=float)
+    data = data.astype(np.float32)
+
+    result = x.run([output_name1], {input_name: data})
+
+    result1 = y.run([output_name1], {input_name1: data})
+
+    return result, result1
+
+def compare_func(result, result2):
+    # 10의 -6승까지 비교
+    return np.count_nonzero((np.abs(result[0]-result2[0])>0.000001).astype(int))
+
+def compare_full_func(result, result2):
+    # 전체 값 비교
+    return np.count_nonzero(result[0] - result2[0])
+
+# ------------------------------------------------------------------------------------------
+# Function Test
+# ------------------------------------------------------------------------------------------
+
+def test_compare():
+    res1, res2 = extract_func(session, session_div1, session_div2)
+    assert compare_func(res1, res2) == 0
+
+def test_compare_part():
+    res1, res2 = extract_part_func(session, session_div1)
+    assert compare_func(res1, res2) == 0
+    assert compare_full_func(res1, res2) == 0
