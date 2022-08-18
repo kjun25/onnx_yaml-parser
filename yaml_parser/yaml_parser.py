5a28a89696c8f2ce967c34fcf7d5a856453c025c
Yun KyungJun <sound605@gmail.com>
2022-08-18 16:41:00 +0900

    Update yaml_parser.py

--- yaml_parser/yaml_parser.py
+++ yaml_parser/yaml_parser.py
@@ -1,9 +1,63 @@
+import argparse
+import os
+
 import yaml
 
-with open('F:/kjYun/2022/07_onnx/config.yml') as file:
+
+# Command line options.
+parser = argparse.ArgumentParser(
+    usage="Helper script to print the histogram from a Glow YAML profile."
+)
+parser.add_argument('--debug', action='store_true', help='debugging mode')
+parser.add_argument(
+    "-f", "--file", dest="file", required=True, type=str, help="Profile YAML file path."
+)
+parser.add_argument(
+    "-n",
+    "--name",
+    dest="name",
+    required=True,
+    type=str,
+    help="Node value name to plot.",
+)
+parser.add_argument(
+    "-l",
+    "--log-scale",
+    dest="log_scale",
+    required=False,
+    default=False,
+    action="store_true",
+    help="Plot the histogram on a logarithmic scale (base 10).",
+)
+
+args = parser.parse_args()
+
+# Get arguments.
+profile = args.file
+name = args.name
+log_scale = args.log_scale
+
+# Verify profile exists.
+if not os.path.isfile(profile):
+    print('File "%s" not found!' % profile)
+    exit(1)
+
+# Read YAML data.
+print('Reading file "%s" ...' % profile)
+data = None
+with open(profile, "r") as stream:
     try:
-        data = yaml.safe_load(file)
-        for key, value in data.items():
-            print(key, ":", value)
-    except yaml.YAMLError as exception:
-        print(exception)
+        data = yaml.safe_load_all(stream)
+    except yaml.YAMLError as err:
+        print(err)
+
+# Search YAML entry for node value.
+    print('Searching node value name "%s" ...' % name)
+    entry = None
+    for item in data:
+        if item["NodeOutputName"] == name:
+            entry = item
+
+    if not entry:
+        print('Node value "%s" not found!' % name)
+        exit(1)
