395ba9035f1c74a7d7a59ff4a1650e4422d6b86a
Yun KyungJun <sound605@gmail.com>
2022-08-18 17:25:59 +0900

    Update yaml_parser.py
    Yaml Parser and Extract onnx

--- yaml_parser/yaml_parser.py
+++ yaml_parser/yaml_parser.py
@@ -1,63 +1,80 @@
 import argparse
 import os
-
 import yaml
-
+import onnx
 
 # Command line options.
 parser = argparse.ArgumentParser(
-    usage="Helper script to print the histogram from a Glow YAML profile."
+    usage="YAML parser for extract onnx"
 )
 parser.add_argument('--debug', action='store_true', help='debugging mode')
+
 parser.add_argument(
-    "-f", "--file", dest="file", required=True, type=str, help="Profile YAML file path."
+    "-x", "--onnx", dest="onnx", required=True, type=str, help="ONNX file path."
 )
+
 parser.add_argument(
-    "-n",
-    "--name",
-    dest="name",
+    "-e", "--ex", dest="extractedOnnx", required=True, type=str, help="Extracted ONNX file path."
+)
+
+parser.add_argument(
+    "-y", "--yaml", dest="yaml", required=True, type=str, help="YAML file path."
+)
+
+parser.add_argument(
+    "-i",
+    "--input",
+    dest="inputName",
     required=True,
     type=str,
-    help="Node value name to plot.",
+    help="NodeInputName.",
 )
 parser.add_argument(
-    "-l",
-    "--log-scale",
-    dest="log_scale",
-    required=False,
-    default=False,
-    action="store_true",
-    help="Plot the histogram on a logarithmic scale (base 10).",
+    "-o",
+    "--output",
+    dest="outputName",
+    required=True,
+    type=str,
+    help="NodeOutputName.",
 )
 
 args = parser.parse_args()
 
 # Get arguments.
-profile = args.file
-name = args.name
-log_scale = args.log_scale
+onnxFile = args.onnx
+extractedOnnxFile = args.extractedOnnx
+yamlFile = args.yaml
+nodeInputName = args.inputName
+nodeOutputName = args.outputName
 
 # Verify profile exists.
-if not os.path.isfile(profile):
-    print('File "%s" not found!' % profile)
+if not os.path.isfile(yamlFile):
+    print('File "%s" not found!' % yamlFile)
     exit(1)
 
 # Read YAML data.
-print('Reading file "%s" ...' % profile)
+print('Reading file "%s" ...' % yamlFile)
 data = None
-with open(profile, "r") as stream:
+with open(yamlFile, "r") as stream:
     try:
         data = yaml.safe_load_all(stream)
     except yaml.YAMLError as err:
         print(err)
 
-# Search YAML entry for node value.
-    print('Searching node value name "%s" ...' % name)
+    # Search YAML entry for node value.
+    print('Searching node value name "%s" ...' % nodeOutputName)
     entry = None
     for item in data:
-        if item["NodeOutputName"] == name:
-            entry = item
-
+        for i in item:
+            if "NodeOutputName" in i:
+                if i["NodeOutputName"].split("__")[0] == nodeOutputName:
+                    entry = i
+                    print(entry)
     if not entry:
-        print('Node value "%s" not found!' % name)
+        print('Node value "%s" not found!' % nodeOutputName)
         exit(1)
+
+print("Input onnx file path: " + onnxFile, "Output extracted onnx file path" + extractedOnnxFile, sep=", ")
+print("Input node name: " + nodeInputName, "Output node name: " + nodeOutputName, sep=", ")
+
+onnx.utils.extract_model(onnxFile, extractedOnnxFile, nodeInputName, nodeOutputName)
