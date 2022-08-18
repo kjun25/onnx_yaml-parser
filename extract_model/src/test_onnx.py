dc91ac0cde69f0611c619a2fc10c0ea80bbe1ee7
Yun KyungJun <sound605@gmail.com>
2022-08-18 18:03:49 +0900

    Fix the code to apply the issue
    https://github.com/kjun25/onnx_yaml-parser/issues/3#issuecomment-1219218019

--- extract_model/src/test_onnx.py
+++ extract_model/src/test_onnx.py
@@ -1,50 +1,206 @@
+import os
+import yaml
+import onnx
+import pytest
+import argparse
 import numpy as np
 import onnxruntime as ort
 
-# test ONNXRuntime functions
-session = ort.InferenceSession('F:/kjYun/2022/07_onnx/mxnet_exported_resnet18.onnx',  providers=['CPUExecutionProvider'])
-session1 = ort.InferenceSession('F:/kjYun/2022/07_onnx/test1.onnx',  providers=['CPUExecutionProvider'])
-session2 = ort.InferenceSession('F:/kjYun/2022/07_onnx/test2.onnx',  providers=['CPUExecutionProvider'])
-session3 = ort.InferenceSession('F:/kjYun/2022/07_onnx/test3.onnx',  providers=['CPUExecutionProvider'])
+# Command line options.
+parser = argparse.ArgumentParser(
+    usage="YAML parser for extract onnx"
+)
+parser.add_argument('--debug', action='store_true', help='debugging mode')
 
-input_name = session.get_inputs()[0].name
-input_shape = session.get_inputs()[0].shape
-output_name = session.get_outputs()[0].name
+parser.add_argument(
+    "-x", "--onnx", dest="onnx", required=True, type=str, help="ONNX file path."
+)
 
-input_name1 = session1.get_inputs()[0].name
-input_shape1 = session.get_inputs()[0].shape
-output_name1 = session1.get_outputs()[0].name
+parser.add_argument(
+    "-e", "--ex", dest="extractedOnnx", required=True, type=str, help="Extracted ONNX file path."
+)
 
-input_name2 = session2.get_inputs()[0].name
-input_shape2 = session2.get_inputs()[0].shape
-output_name2 = session2.get_outputs()[0].name
+parser.add_argument(
+    "-y", "--yaml", dest="yaml", required=True, type=str, help="YAML file path."
+)
 
-input_name3 = session3.get_inputs()[0].name
-input_shape3 = session3.get_inputs()[0].shape
-output_name3 = session3.get_outputs()[0].name
+parser.add_argument(
+    "-i",
+    "--input",
+    dest="inputName",
+    required=True,
+    type=str,
+    help="NodeInputName.",
+)
+parser.add_argument(
+    "-o",
+    "--output",
+    dest="outputName",
+    required=True,
+    type=str,
+    help="NodeOutputName.",
+)
 
-x = np.ones(input_shape, dtype=float)
-x = x.astype(np.float32)
+args = parser.parse_args()
 
-result = session.run([output_name], {input_name: x})
-print("[mxnet_exported_resnet18] input_name: " + input_name)
-print("[mxnet_exported_resnet18] output_name: " + output_name)
+# Get arguments.
+onnxFile = args.onnx
+extractedOnnxFile = args.extractedOnnx
+yamlFile = args.yaml
+nodeInputName = args.inputName
+nodeOutputName = args.outputName
 
-x1 = np.ones(input_shape1, dtype=float)
-x1 = x1.astype(np.float32)
+# Verify profile exists.
+if not os.path.isfile(yamlFile):
+    print('File "%s" not found!' % yamlFile)
+    exit(1)
 
-result1 = session1.run([output_name1], {input_name1: x1})
-print("[divided_resnet18_part1] input_name: " + input_name1)
-print("[divided_resnet18_part1] output_name: " + output_name1)
+# Read YAML data.
+print('Reading file "%s" ...' % yamlFile)
+data = None
+with open(yamlFile, "r") as stream:
+    try:
+        data = yaml.safe_load_all(stream)
+    except yaml.YAMLError as err:
+        print(err)
 
-result2 = session2.run([output_name2], {input_name2: result1[0]})
-print("[divided_resnet18_part2] input_name: " + input_name2)
-print("[divided_resnet18_part2] output_name: " + output_name2)
+    # Search YAML entry for node value.
+    print('Searching node value name "%s" ...' % nodeOutputName)
+    entry = None
+    for item in data:
+        for i in item:
+            if "NodeOutputName" in i:
+                splitValue = i["NodeOutputName"].split("__")[0]
+                if splitValue == nodeOutputName:
+                    entry = splitValue
+                    print(entry)
+    if not entry:
+        print('Node value "%s" not found!' % nodeOutputName)
+        exit(1)
 
-result3 = session3.run([output_name3], {input_name3: result2[0]})
-print("[divided_resnet18_part3] input_name: " + input_name3)
-print("[divided_resnet18_part3] output_name: " + output_name3)
+print("Input onnx file path: " + onnxFile, "Output extracted onnx file path" + extractedOnnxFile, sep=", ")
+print("Input node name: " + nodeInputName, "Output node name: " + entry, sep=", ")
 
-result = np.array_equal(result, result3)
+# Extract onnx model.
+onnx.utils.extract_model(onnxFile, extractedOnnxFile, nodeInputName, entry)
 
-print(result)
+# ONNXRuntime functions
+session = ort.InferenceSession(onnxFile, providers=['CPUExecutionProvider'])
+session_div1 = ort.InferenceSession(extractedOnnxFile, providers=['CPUExecutionProvider'])
+session_div2 = ort.InferenceSession('test2.onnx', providers=['CPUExecutionProvider'])
+
+
+def extract_func(x, y, z):
+    input_name = x.get_inputs()[0].name
+    input_shape = x.get_inputs()[0].shape
+    input_type = x.get_inputs()[0].type
+    output_name = x.get_outputs()[0].name
+
+    input_name1 = y.get_inputs()[0].name
+    input_shape1 = y.get_inputs()[0].shape
+    input_type1 = y.get_inputs()[0].type
+    output_name1 = y.get_outputs()[0].name
+
+    input_name2 = z.get_inputs()[0].name
+    input_shape2 = z.get_inputs()[0].shape
+    input_type2 = z.get_inputs()[0].type
+    output_name2 = z.get_outputs()[0].name
+
+    data = np.ones(input_shape, dtype=float)
+    data = data.astype(np.float32)
+
+    result = x.run([output_name], {input_name: data})
+
+    result1 = y.run([output_name1], {input_name1: data})
+
+    result2 = z.run([output_name2], {input_name2: result1[0]})
+
+    return result, result2
+
+
+def extract_part_func(x, y):
+    input_name = x.get_inputs()[0].name
+    input_shape = x.get_inputs()[0].shape
+    input_type = x.get_inputs()[0].type
+    output_name = x.get_outputs()[0].name
+
+    input_name1 = y.get_inputs()[0].name
+    input_shape1 = y.get_inputs()[0].shape
+    input_type1 = y.get_inputs()[0].type
+    output_name1 = y.get_outputs()[0].name
+
+    data = np.ones(input_shape, dtype=float)
+    data = data.astype(np.float32)
+
+    result = x.run([output_name1], {input_name: data})
+
+    result1 = y.run([output_name1], {input_name1: data})
+
+    return result, result1
+
+
+def compareValue_func(result, result2):
+    # 10의 -6승까지 비교
+    return np.count_nonzero((np.abs(result[0] - result2[0]) > 0.000001).astype(int))
+
+
+def compareFull_func(result, result2):
+    # 전체 값 비교
+    return np.count_nonzero(result[0] - result2[0])
+
+
+def compare_inputName(session, session_div1):
+    return session.get_inputs()[0].name, session_div1.get_inputs()[0].name
+
+
+def compare_inputShape(session, session_div1):
+    return session.get_inputs()[0].shape, session_div1.get_inputs()[0].shape
+
+
+def compare_inputType(session, session_div1):
+    return session.get_inputs()[0].type, session_div1.get_inputs()[0].type
+
+
+def compare_outputName(session, session_div2):
+    return session.get_outputs()[0].name, session_div2.get_outputs()[0].name
+
+
+def compare_outputShape(session, session_div2):
+    return session.get_outputs()[0].shape, session_div2.get_outputs()[0].shape
+
+
+def compare_outputType(session, session_div2):
+    return session.get_outputs()[0].type, session_div2.get_outputs()[0].type
+
+
+# ------------------------------------------------------------------------------------------
+# Function Test
+# ------------------------------------------------------------------------------------------
+
+def test_compareValue():
+    res1, res2 = extract_func(session, session_div1, session_div2)
+    assert compareValue_func(res1, res2) == 0
+
+
+def test_compare_inputParameters():
+    nameResult1, nameResult2 = compare_inputName(session, session_div1)
+    shapeResult1, shapeResult2 = compare_inputShape(session, session_div1)
+    typeResult1, typeResult2 = compare_inputType(session, session_div1)
+
+    assert nameResult1 == nameResult2
+    assert shapeResult1 == shapeResult2
+    assert typeResult1 == typeResult2
+
+
+def test_compare_outputParameters():
+    nameResult1, nameResult2 = compare_outputName(session, session_div2)
+    shapeResult1, shapeResult2 = compare_outputShape(session, session_div2)
+    typeResult1, typeResult2 = compare_outputType(session, session_div2)
+
+    assert nameResult1 == nameResult2
+    assert shapeResult1 == shapeResult2
+    assert typeResult1 == typeResult2
+
+
+if __name__ == "__main__":
+    pytest.main(args)
