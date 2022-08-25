f06a1bbaaae33c4f32951eb76339d0d1abbd3270
Yun KyungJun <sound605@gmail.com>
2022-08-26 03:21:32 +0900

    Delete extract_model.py

--- extract_model/src/extract_model.py
+++
@@ -1,8 +0,0 @@
-import onnx
-
-input_path = 'F:/kjYun/2022/07_onnx/mxnet_exported_resnet18.onnx'
-output_path = 'F:/kjYun/2022/07_onnx/testCheck.onnx'
-input_names = ['data']
-output_names = ['resnetv10_relu0_fwd']
-
-onnx.utils.extract_model(input_path, output_path, input_names, output_names)
