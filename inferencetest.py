f9b37f0c1bee6751d991fdb427c14c1d0cc43c03
Jemin Lee <leejaymin@etri.re.kr>
2022-09-06 16:24:27 +0900

    Add imagenet normalization.

--- inferencetest.py
+++ inferencetest.py
@@ -38,6 +38,14 @@ def softmax(a):
 
     return y
 
+def imagenet_preprocess(img_data):
+    mean_vec = np.array([0.485, 0.456, 0.406])
+    stddev_vec = np.array([0.229, 0.224, 0.225])
+    norm_img_data = np.zeros(img_data.shape).astype('float32')
+    for i in range(img_data.shape[0]):
+         # for each pixel in each channel, divide the value by 255 to get value between [0, 1] and then normalize
+        norm_img_data[i,:,:] = (img_data[i,:,:]/255 - mean_vec[i]) / stddev_vec[i]
+    return norm_img_data
 
 def main(args):
     img = cv2.imread(args.image).astype(np.float32)
@@ -47,12 +55,16 @@ def main(args):
     img = np.expand_dims(img, axis=0)
     print("input Image.shape: ", img.shape)
 
+    img_arr = imagenet_preprocess(img)
+    print("input Image.shape: ", img_arr.shape)
+
+
     session = ort.InferenceSession(args.onnx, providers=['CPUExecutionProvider'])
 
     input_name = session.get_inputs()[0].name
     output_name = session.get_outputs()[0].name
 
-    out = session.run([output_name], {input_name: img})[0]
+    out = session.run([output_name], {input_name: img_arr})[0]
     result = softmax(out[0])
     print("sum of softmax value: ", sum(softmax(out[0])))
     sortedOutput = np.sort(result)[::-1]
