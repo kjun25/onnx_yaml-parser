e16179388ffb3c98e8190f9a7d0e02a04b25b6e0
Yun KyungJun <sound605@gmail.com>
2022-09-16 17:02:36 +0900

    Update yaml_parser.py
    Fixed errors when partitioning into input name
    Add segmentation by node name

--- yaml_parser/yaml_parser.py
+++ yaml_parser/yaml_parser.py
@@ -1,82 +1,162 @@
-import argparse
 import os
 import yaml
 import onnx
+import argparse
+import onnxruntime as ort
+import numpy as np
 
-# Command line options.
-parser = argparse.ArgumentParser(
-    usage="YAML parser for extract onnx"
-)
-parser.add_argument('--debug', action='store_true', help='debugging mode')
 
-parser.add_argument(
-    "-x", "--onnx", dest="onnx", required=True, type=str, help="ONNX file path."
-)
+def parse_args():
+    parser = argparse.ArgumentParser(description='Parsing yaml to extract onnx model')
 
-parser.add_argument(
-    "-e", "--ex", dest="extractedOnnx", required=True, type=str, help="Extracted ONNX file path."
-)
+    parser.add_argument('--debug', action='store_true', help='Debugging mode')
 
-parser.add_argument(
-    "-y", "--yaml", dest="yaml", required=True, type=str, help="YAML file path."
-)
+    parser.add_argument(
+        '--onnx_file',
+        dest='onnx_file',
+        help='The onnx model path.',
+        type=str,
+        default=None,
+        required=True
+    )
 
-parser.add_argument(
-    "-i",
-    "--input",
-    dest="inputName",
-    required=True,
-    type=str,
-    help="NodeInputName.",
-)
-parser.add_argument(
-    "-o",
-    "--output",
-    dest="outputName",
-    required=True,
-    type=str,
-    help="NodeOutputName.",
-)
+    parser.add_argument(
+        '--yaml_dir',
+        dest='yaml_dir',
+        help='The yaml file path.',
+        type=str,
+        default=None,
+        required=True
+    )
 
-args = parser.parse_args()
+    parser.add_argument(
+        '--save_dir',
+        dest='save_dir',
+        help='The directory for saving the extract model.',
+        type=str,
+        default='./output'
+    )
+    return parser.parse_args()
 
-# Get arguments.
-onnxFile = args.onnx
-extractedOnnxFile = args.extractedOnnx
-yamlFile = args.yaml
-nodeInputName = args.inputName
-nodeOutputName = args.outputName
 
-# Verify profile exists.
-if not os.path.isfile(yamlFile):
-    print('File "%s" not found!' % yamlFile)
-    exit(1)
+def extract_model(onnx_path, yaml_dir_path, save_dir_path):
+    model = onnx.load(onnx_path)
+    input_all = []
 
-# Read YAML data.
-print('Reading file "%s" ...' % yamlFile)
-data = None
-with open(yamlFile, "r") as stream:
-    try:
-        data = yaml.safe_load_all(stream)
-    except yaml.YAMLError as err:
-        print(err)
+    for input in model.graph.node:
+        input_all.append(input.input)
+    output_all = [output.name for output in model.graph.node]
 
-    # Search YAML entry for node value.
-    print('Searching node value name "%s" ...' % nodeOutputName)
-    entry = None
-    for item in data:
-        for i in item:
-            if "NodeOutputName" in i:
-                splitValue = i["NodeOutputName"].split("__")[0]
-                if splitValue == nodeOutputName:
-                    entry = splitValue
-                    print(entry)
-    if not entry:
-        print('Node value "%s" not found!' % nodeOutputName)
-        exit(1)
+    file_list = os.listdir(yaml_dir_path)
+    file_list_yaml = [file for file in file_list if file.endswith(".yaml")]
 
-print("Input onnx file path: " + onnxFile, "Output extracted onnx file path" + extractedOnnxFile, sep=", ")
-print("Input node name: " + nodeInputName, "Output node name: " + entry, sep=", ")
+    # Read YAML data.
+    print('Reading YAML file "%s"' % file_list_yaml)
+    data = None
+    for file in file_list_yaml:
+        with open(yaml_dir_path + file, "r") as stream:
+            try:
+                data = yaml.safe_load_all(stream)
+                print(file)
+            except yaml.YAMLError as err:
+                print(err)
 
-# Extract onnx model.
-onnx.utils.extract_model(onnxFile, extractedOnnxFile, nodeInputName, entry)
+            # Search YAML entry for node value.
+            partition = []
+            for item in data:
+                for idx, node in enumerate(item):
+                    if "NodeOutputName" in node:
+                        splitValue1 = node["NodeOutputName"].split("__")[0]
+                        splitValue2 = splitValue1.split(":0")[0]
+                        partition.append(splitValue2)
+            n = []
+            for i in output_all:
+                if i in partition:
+                    n.append(i)
+
+            m = list(set(output_all) & set(partition))[0]
+            for i in list(set(output_all) & set(partition)):
+                if output_all.index(i) > output_all.index(m):
+                    m = i
+
+            if output_all.index(m) > len(partition):
+                print('The index of a node "%d" cannot be greater than list size "%d"!' % (
+                    output_all.index(m), len(partition)))
+                exit(1)
+
+            k = []
+            for input in model.graph.node:
+                if input.name in n:
+                    k.append(input.input)
+                    
+            # Extract onnx model.
+
+            input_names = k[0]
+            output_names = [m]
+            print(input_names)
+            print(output_names)
+            onnx.utils.extract_model(onnx_path, save_dir_path + file.split(".yaml")[0] + '.onnx', input_names,
+                                     output_names)
+
+
+def read_onnx(onnx_path, extracted_onnx_path):
+    file_list = os.listdir(extracted_onnx_path)
+    file_list_onnx = [file for file in file_list if file.endswith(".onnx")]
+
+    session = ort.InferenceSession(onnx_path, providers=['CPUExecutionProvider'])
+
+    input_name = session.get_inputs()[0].name
+    input_shape = session.get_inputs()[0].shape
+    output_name = session.get_outputs()[0].name
+
+    data = np.ones(input_shape, dtype=float)
+    data = data.astype(np.float32)
+
+    result = session.run([output_name], {input_name: data})
+    sessions = []
+    # ONNXRuntime functions
+
+    for file in file_list_onnx:
+        sess = ort.InferenceSession(extracted_onnx_path + file, providers=['CPUExecutionProvider'])
+        sessions.append(sess)
+
+    result_data = []
+    for sess in sessions:
+        if sessions.index(sess) == 0:
+            input_name = sess.get_inputs()[0].name
+            output_name = sess.get_outputs()[0].name
+            result_data = sess.run([output_name], {input_name: data})
+        else:
+            input_name = sess.get_inputs()[0].name
+            output_name = sess.get_outputs()[0].name
+            result_data = sess.run([output_name], {input_name: result_data[0]})
+
+    return result, result_data
+
+
+def compareValue_func(result, result2):
+    # 10의 -6승까지 비교
+    return np.count_nonzero((np.abs(result[0] - result2[0]) > 0.000001).astype(int))
+
+
+def main(args):
+    # Extract ONNX model
+    extract_model(args.onnx_file, args.yaml_dir, args.save_dir)
+
+    # Read ONNX models
+    res1, res2 = read_onnx(args.onnx_file, args.save_dir)
+    print(np.count_nonzero((np.abs(res1[0] - res2[0]) > 0.00001).astype(int)))
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
+
+
+# ------------------------------------------------------------------------------------------
+# Function Test
+# ------------------------------------------------------------------------------------------
+
+def test_compareValue():
+    res1, res2 = read_onnx(args.onnx_file, args.save_dir)
+    assert compareValue_func(res1, res2) == 0
